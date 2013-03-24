38e087c5e6ebab7214e2e0d7975878745728bc5a
Michiel Borkent <michielborkent@gmail.com>
2013-03-24 18:15:59 +0100

    changed project.clj towards compojure/lib-noir

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
-(defproject tictactoe "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.0"]]
-            :main tictactoe.server)
+(defproject tictactoe "0.1.1-SNAPSHOT"
+  :description "Tictactoe using ring, compojure, lib-noir and hiccup"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [lib-noir "0.4.9"]
+                 [compojure "1.1.5"]
+                 [ring-server "0.2.7"]
+                 [hiccup "1.0.2"]]
+  :ring {:handler tictactoe/handler.war-handler,
+         :init tictactoe.handler/init,
+         :destroy tictactoe.handler/destroy}
+  :profiles {:production
+             {:ring
+              {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+             :dev
+             {:dependencies [[ring-mock "0.1.3"] [ring/ring-devel "1.1.8"]]}}
+  :url "http://example.com/FIXME"
+  :plugins [[lein-ring "0.8.3"]]
+  :min-lein-version "2.0.0")
 
